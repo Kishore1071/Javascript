3c848df9bc8dc74d5e21ecbb5f9f9ae6be9a2c27
Kishore1071 <kishore240398@gmail.com>
2024-01-10 09:50:42 +0530

    Updated

---
+++ Basics/Task.js
@@ -0,0 +1,53 @@
+// PRIME NUMBER
+
+let i
+let prime_numbers = []
+
+for (i = 2; i <= 100; i++) {
+
+    let n = 2
+
+    let prime = true
+
+    while (n < i) {
+
+        value = i % n
+
+        if (value === 0) {
+
+            prime = false
+
+            break
+        }
+
+        n = n + 1
+    }
+
+    if (prime === true) {
+        prime_numbers.push(i)
+    }   
+}
+
+console.log(prime_numbers)
+
+
+// FIBONACCi
+
+let number1 = 0
+let number2 = 1
+let count = 0
+
+while (count < 20) {
+    if (count === 0) {
+        console.log(number1)
+        console.log(number2)
+    }
+    else {
+        let value = number1 + number2
+        console.log(value);
+        number1 = number2
+        number2 = value
+    }
+
+    count = count + 1
+}
---
+++ Basics/test.js
@@ -0,0 +1,10 @@
+// const LogData = () => {
+//     const username = document.getElementById('username').value
+//     // console.log(username)
+//     window.location.href = 'second.html'
+//
+
+const ShowData = () => {
+
+    document.getElementById('data').innerHTML= 'hi'
+}
